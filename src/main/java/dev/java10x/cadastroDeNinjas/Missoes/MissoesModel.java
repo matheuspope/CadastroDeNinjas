911c414c1a0ded3658c8a1491a3507eaf6845c71
Matheus Pope <matheus.pope1@gmail.com>
2026-09-01 16:29:30 -0300

    feat: Puts Lombok in the class for abstraction

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -4,10 +4,18 @@ package dev.java10x.cadastroDeNinjas.Missoes;
 import dev.java10x.cadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -30,28 +38,5 @@ public class MissoesModel {
 
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 
 }
--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.cadastroDeNinjas.Ninjas.Controller.Service;
 
 import dev.java10x.cadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,10 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
     @Id
@@ -26,37 +33,5 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
