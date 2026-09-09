d0bfc4ce87ed78001e7f7ba921af18849d9bae2d
Matheus Pope <matheus.pope1@gmail.com>
2026-09-09 14:16:26 -0300

    fix: Add @Collumn to my class

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -20,15 +20,21 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
+
     // @ManyToOne um ninja tem uma única missao
     @ManyToOne
     @JoinColumn(name = "missoes_id") //Fereing Key ou chave estranheira
